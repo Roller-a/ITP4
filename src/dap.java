13fed6b0716864158184111801e722bfcd40dc98
Artemii Mashanov <ms.mashanov@mail.ru>
2024-11-12 15:44:16 +0300

    Initial commit

---
+++ src/dap.java
@@ -0,0 +1,11 @@
+class dap extends dop{
+    @Override
+    void func(){
+        System.out.println("func");
+    }
+
+    @Override
+    void pu() {
+        System.out.println("pu");
+    }
+}
---
+++ src/dop.java
@@ -0,0 +1,6 @@
+abstract class dop {
+    void func() {
+        System.out.println("hello");
+    }
+    abstract void pu();
+}
